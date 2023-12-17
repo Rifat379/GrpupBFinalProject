1b68614933eb3c12c32c814c9098038e6cf81cc3
Rifat379 <rifat.ritu379@gmail.com>
2023-12-17 10:16:48 -0500

    test

--- src/test/java/FidelisCareTest/TestCase1.java
+++ src/test/java/FidelisCareTest/TestCase1.java
@@ -3,12 +3,14 @@ package FidelisCareTest;
 import FidelisCare.Homepage;
 import common.WebAPI;
 import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.Test;
 
 public class TestCase1 extends WebAPI {
     static Homepage homepage;
     public static void getInItElements(){
         homepage= PageFactory.initElements(driver, Homepage.class);
     }
+    @Test
     public void test(){
         getInItElements();
         homepage.performActions();
